3b3a8a8a5a296fe20b2c0141b07a91378f793847
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-15 00:33:21 -0400

    Create a class containing similarly functioning code

--- sketch.js
+++ sketch.js
@@ -35,3 +35,34 @@ function redirect() {
   x = x + speed.x;
   y = y + speed.y;
 };
+
+class Bounce {
+  constructor(){
+    this.x = 100;
+    this.y = 100;
+    this.speed = {
+      x: 50,
+      y: 10
+    };
+  }
+
+  paint() {
+    fill(0);
+    stroke(255);
+    ellipse(this.x, this.y, 100, 100);
+  };
+
+  move() {
+    if(this.x >= width || this.x < 0) {
+      this.speed.x = (-this.speed.x);
+    }
+    if(this.y >= height || this.y < 0) {
+      this.speed.y = (-this.speed.y);
+    }
+  };
+
+  redirect() {
+    this.x = this.x + this.speed.x;
+    this.y = this.y + this.speed.y;
+  };
+}
