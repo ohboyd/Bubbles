b31977abe3ae5d9f53a961244d83e2c6f6c52bc1
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-15 00:25:12 -0400

    Add bouncing motion to the circle

--- sketch.js
+++ sketch.js
@@ -1,3 +1,10 @@
+let x = 100;
+let y = 100;
+let speed = {
+  x: 50,
+  y: 10
+};
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 }
@@ -6,5 +13,15 @@ function draw() {
   background(0);
   fill(0);
   stroke(255);
-  ellipse((width/2), (height/2), 300, 300);
+  ellipse(x, y, 100, 100);
+
+  if(x >= width || x < 0) {
+    speed.x = (-speed.x);
+  };
+  if(y >= height || y < 0) {
+    speed.y = (-speed.y);
+  };
+
+  x = x + speed.x;
+  y = y + speed.y;
 }
