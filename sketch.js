d9cbee258c5aaa4dc9a630fe970034e48cdc6664
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-15 00:19:09 -0400

    Create bubble to use for motion tests

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+  background(0);
+  fill(0);
+  stroke(255);
+  ellipse((width/2), (height/2), 300, 300);
+}
