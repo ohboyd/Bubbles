60f72cc74d467c8bdfedd3779110dc7f00b67fe9
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-15 00:26:17 -0400

    Extract similar code for motion and bouncing to individual functions

--- sketch.js
+++ sketch.js
@@ -14,14 +14,20 @@ function draw() {
   fill(0);
   stroke(255);
   ellipse(x, y, 100, 100);
+  move();
+  redirect();
+}
 
+function move() {
   if(x >= width || x < 0) {
     speed.x = (-speed.x);
-  };
+  }
   if(y >= height || y < 0) {
     speed.y = (-speed.y);
-  };
+  }
+};
 
+function redirect() {
   x = x + speed.x;
   y = y + speed.y;
-}
+};
