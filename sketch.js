1fd31b07480dda0f0b5c97e9272acf44e2810c74
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-15 00:34:03 -0400

    Remove duplicated code

--- sketch.js
+++ sketch.js
@@ -1,40 +1,13 @@
-let x = 100;
-let y = 100;
-let speed = {
-  x: 50,
-  y: 10
-};
-
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
 }
 
 function draw() {
   background(0);
-  paint();
-  move();
-  redirect();
+  // paint();
+  // move();
+  // redirect();
 }
-
-function paint() {
-  fill(0);
-  stroke(255);
-  ellipse(x, y, 100, 100);
-};
-
-function move() {
-  if(x >= width || x < 0) {
-    speed.x = (-speed.x);
-  }
-  if(y >= height || y < 0) {
-    speed.y = (-speed.y);
-  }
-};
-
-function redirect() {
-  x = x + speed.x;
-  y = y + speed.y;
-};
 
 class Bounce {
   constructor(){
