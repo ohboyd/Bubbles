a0c0ba2991a5175c9be194172de9a6f3ade56e24
Mike Boyd <mike.boyd@radicalbear.com>
2019-08-15 20:51:41 -0400

    Add iterator to create more objects when dragging the mouse

--- bounce.js
+++ bounce.js
@@ -1,30 +1,30 @@
 class Bounce {
-  constructor(xSpeed, ySpeed){
-    this.x = 100;
-    this.y = 100;
+  constructor(xLocation, yLocation){
+    this.x = xLocation;
+    this.y = yLocation;
     this.speed = {
-      x: xSpeed,
-      y: ySpeed
+      x: random(-2, 2),
+      y: random(-2, 2)
     };
   }
 
   paint() {
-    fill(0);
+    fill(0, 150, 150);
     stroke(255);
-    ellipse(this.x, this.y, 100, 100);
+    ellipse(this.x, this.y, 50);
   };
 
   move() {
+    this.x = this.x + this.speed.x;
+    this.y = this.y + this.speed.y;
+  };
+
+  redirect() {
     if(this.x >= width || this.x < 0) {
       this.speed.x = (-this.speed.x);
     }
     if(this.y >= height || this.y < 0) {
       this.speed.y = (-this.speed.y);
     }
-  };
-
-  redirect() {
-    this.x = this.x + this.speed.x;
-    this.y = this.y + this.speed.y;
   };
 }
--- sketch.js
+++ sketch.js
@@ -1,20 +1,18 @@
+let balls = [];
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  let randomNumber = random(1, 50);
-  ball1 = new Bounce(randomNumber, 10);
-  ball2 = new Bounce(5, randomNumber);
-  ball3 = new Bounce(30, 4);
+}
+
+function mouseDragged() {
+  let b = new Bounce(mouseX, mouseY)
+  balls.push(b);
 }
 
 function draw() {
   background(0);
-  ball1.paint();
-  ball1.move();
-  ball1.redirect();
-  ball2.paint();
-  ball2.move();
-  ball2.redirect();
-  ball3.paint();
-  ball3.move();
-  ball3.redirect();
+  for (var i = 0; i < balls.length; i++) {
+    balls[i].paint();
+    balls[i].move();
+  }
 }
